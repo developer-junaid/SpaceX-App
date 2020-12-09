347d954c6eb7aa10d67e3115041c9218b8029e74
developer-junaid <qjunaid623@gmail.com>
2020-12-09 23:38:20 +0500

    Next Add API Data

---
+++ src/Data/Launch/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useLaunchInfoQuery } from "../../generated/graphql";
+import MissionInfoList from "./MissionInfoList";
+
+const MissionInfo = () => {
+  // Get Launch Data
+  const { data, loading, error } = useLaunchInfoQuery({
+    variables: { id: "25" },
+  });
+
+  // If Loading
+  if (loading) {
+    return <h1>Loading</h1>;
+  }
+
+  // If Error
+  if (error || !data) {
+    return <h1>Error</h1>;
+  }
+
+  // If All Good
+  return <MissionInfoList data={data} />;
+};
+
+export default MissionInfo;
